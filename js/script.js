f92628d1f49d5d2dc5a659c44599a578562148f6
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-17 16:00:08 -0300

    feat: Adiciona tema claro/escuro utilizando js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const rua = document.getElementById('rua');
 const bairro = document.getElementById('bairro');
 const cidade = document.getElementById('cidade');
 const estado = document.getElementById('estado');
+const tema = document.getElementById('dark-theme')
+const temaIcon = tema.querySelector('img')
 
 async function buscarCep() {
     const cepValor = cep.value.replace(/\D/g, '');
@@ -34,6 +36,32 @@ function limparFormulario() {
     cidade.value = "";
     estado.value = "";
 }
+
+function atualizarIcon(isDark) {
+    if (isDark) {
+        temaIcon.setAttribute("src", "images/sun.svg")
+        temaIcon.setAttribute("light-theme");
+    } else {
+        temaIcon.setAttribute("src", "images/moon.svg")
+        temaIcon.setAttribute("dark-theme");
+    }
+}
+
+if(localStorage.getItem("tema") === "dark"){
+    document.body.classList.add("dark-theme");
+    atualizarIcon(true);
+}
+
+tema.addEventListener("click", function(event){
+    event.preventDefault();
+
+    document.body.classList.toggle("dark-theme");
+    const isDark = document.body.classList.contains("dark-theme");
+
+    localStorage.setItem("tema", isDark ? "escuro" : "claro");
+
+    atualizarIcon(isDark);
+})
 
 
 cep.addEventListener('keydown', function (event) {
